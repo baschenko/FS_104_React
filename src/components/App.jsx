11094fb3fd5dce3e9d0cd01e945b98e7868451da
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-06 18:02:47 +0300

    init m2l1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,12 @@
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import '../index.css';
-import Profile from './Profile/Profile';
-import profileData from '../assets/user.json';
-import s from './App.module.css';
-import clsx from 'clsx';
-import Modal from './Modal/Modal';
-
+import { ColorPicker } from './ColorPicker/ColorPicker';
+import { Counter } from './Counter/Counter';
+import { TodoList } from './TodoList/TodoList';
 export const App = () => {
-  const age = 21;
-  const fruits = ['banana', 'apple', 'lemon'];
   return (
-    <>
-      <Header />
-      <h2 className="title">Hello styles</h2>
-      <Profile user={profileData} />
-
-      {age > 18 ? <h2>Ого, який дорослий!!</h2> : <h2>Треба ще підрости</h2>}
-      <ul>
-        {fruits.map(item => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-      <Footer />
-      <div className={s.wrapper}>
-        <div className={clsx(s.box)}></div>
-        <div className={clsx(s.red)}></div>
-        <div className={clsx(s.box)}></div>
-        <div className={clsx(s.black)}></div>
-      </div>
-      <Modal>
-        <p>Hello</p>
-      </Modal>
-    </>
+    <div>
+      <Counter />
+      <TodoList />
+      <ColorPicker />
+    </div>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,15 @@
-const Modal = ({ children }) => {
+import s from './Modal.module.css';
+const Modal = ({ children, title = 'Default modal' }) => {
   return (
-    <div>
-      <h2>title Modal</h2>
-      {children}
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <>
+          <h1>{title}</h1>
+          <hr />
+        </>
+        <button className={s.closeBtn}>×</button>
+        {children}
+      </div>
     </div>
   );
 };
