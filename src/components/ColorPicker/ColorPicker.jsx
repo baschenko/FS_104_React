c9afb037d8e0e6036ad7048721c3e3707aa0c4e8
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-06 20:10:35 +0300

    colorPicker

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,12 +1,26 @@
 import s from './ColorPicker.module.css';
 import colors from '../../assets/colors.json';
+import { useState } from 'react';
+
 export const ColorPicker = () => {
+  const [currentColor, setCurrentColor] = useState('white');
+
+  const handleClick = color => {
+    console.log(color);
+    setCurrentColor(color);
+  };
+
   return (
-    <section className={s.bgWrapper}>
+    <section className={s.bgWrapper} style={{ backgroundColor: currentColor }}>
       <div className={s.pallette}>
+        <h1>Current color: {currentColor}</h1>
         <ul className={s.list}>
           {colors.map(item => (
-            <li className={s.item} key={item.id}>
+            <li
+              className={s.item}
+              key={item.id}
+              onClick={() => handleClick(item.color)}
+            >
               {item.color}
             </li>
           ))}
