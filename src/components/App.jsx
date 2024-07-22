71c64e40c6a05896e60a8ac70f99e844004f4154
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-23 00:40:29 +0300

    error handlin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,16 +8,19 @@ const App = () => {
   const [hits, setHits] = useState([]);
   const [query, setQuery] = useState('react');
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
       try {
         setIsLoading(true);
+        setIsError(false);
         const response = await fetchNews(query, 25);
 
         setHits(response.hits);
       } catch (error) {
         console.log(error);
+        setIsError(true);
       } finally {
         setIsLoading(false);
       }
@@ -31,6 +34,7 @@ const App = () => {
       <SearchBar setQuery={setQuery} />
       <List items={hits} />
       {isLoading && <Loader />}
+      {isError && <h2>Something went wrong! Try again...</h2>}
     </div>
   );
 };
