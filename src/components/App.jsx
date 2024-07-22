b2451a93c21d07186cc2383685e364ac94ce2415
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-22 23:54:13 +0300

    add search bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 import List from './List/List';
 import { fetchNews } from '../services/api';
+import SearchBar from './SearchBar/SearchBar';
 
 const App = () => {
   const [hits, setHits] = useState([]);
+  const [query, setQuery] = useState('react');
+
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await fetchNews('react', 25);
+        const response = await fetchNews(query, 25);
         setHits(response.hits);
       } catch (error) {
         console.log(error);
@@ -15,10 +18,11 @@ const App = () => {
     };
 
     getData();
-  }, []);
+  }, [query]);
 
   return (
     <div>
+      <SearchBar setQuery={setQuery} />
       <List items={hits} />
     </div>
   );
