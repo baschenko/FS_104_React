4b92e3b02839836262462e857c6589edc4a586cb
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-31 22:56:04 +0300

    m5l1 init

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import { App } from './components/App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <>
     <App />
-  </Provider>
+  </Brow>
 );
