0cda7c7202a5f7a283a33c15c82c33478fdf39fa
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-31 23:23:28 +0300

    instal router and create routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import { App } from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <BrowserRouter>
     <App />
-  </Brow>
+  </BrowserRouter>
 );
